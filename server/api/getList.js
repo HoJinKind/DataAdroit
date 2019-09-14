c765b2e506772f51a90c26a470cde84a937f33dd
MikeHo <jinkind_ho@mymail.sutd.edu.sg>
2019-09-14 11:17:07 +0800

    add xml query

--- server/api/getList.js
+++ server/api/getList.js
@@ -2,10 +2,33 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
+var parser = require('xml2json');
+var Transform = require('stream').Transform;
+
 router.get('/', function(req, res, next) {
-  request('http://apidintegra.tkfweb.com:80/apid/request?method=login&ci=apiD&ui=SG21701-f10apid13&pwd=f10apid_13'
-  ).pipe(res);
-  console.log(res);
+  login(res)
 });
 
 module.exports = router;
+
+// var http = require('http');
+// var fs = require('fs');
+// var parser = require('xml2json');
+// var Transform = require('stream').Transform;
+function login(res){
+  request('http://apidintegra.tkfweb.com:80/apid/request?method=login&ci=apiD&ui=SG21701-f10apid13&pwd=f10apid_13',{},(err,response,body)=>{
+    console.log(response.body)
+    a = f10XML(response.body)// this is the id token, 
+    res.json(a)
+  })
+}
+function f10XML(rawXML){
+  console.log(JSON.parse(parser.toJson(rawXML)))
+  return JSON.parse(parser.toJson(rawXML)).XRF.A.v
+}
+
+// var server = http.createServer(function (req, res) {
+//   var stream = fs.createReadStream(__dirname + '/data.xml');
+//   stream.pipe(xmlParser()).pipe(res);
+// });
+// server.listen(8000);
